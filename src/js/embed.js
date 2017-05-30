ad10b8691243f073691b9d15f2d731fa184e638f
Garry Blight <garry.blight@guardian.co.uk>
2017-05-30 12:44:26 +0100

    Data connected

--- src/js/embed.js
+++ src/js/embed.js
@@ -2,15 +2,43 @@ import iframeMessenger from 'guardian/iframe-messenger'
 import reqwest from 'reqwest'
 import embedHTML from './text/embed.html!text'
 
+var mode = null, data = null;
+
 window.init = function init(el, config) {
     iframeMessenger.enableAutoResize();
 
     el.innerHTML = embedHTML;
 
     reqwest({
-        url: 'http://ip.jsontest.com/',
+        url: 'https://interactive.guim.co.uk/docsdata/1lrMs23YPKWxRStLVLiVj407bvIJvlG_WZxBMkVYui6I.json',
         type: 'json',
         crossOrigin: true,
-        success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
+        success: (resp) => buildApp(resp)
     });
 };
+
+function buildApp(resp) {
+
+    mode= getParameterByName("mode");
+    data = resp.sheets.Sheet1;
+
+    if (mode === null) {
+        mode = "full";
+    }
+
+    console.log(mode);
+    console.log(data);
+    
+}
+
+ function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
