7dac1b8116c7549c22b3167ad75cfdc3d93e7db6
Garry Blight <garry.blight@guardian.co.uk>
2017-06-01 14:59:13 +0100

    Code format

--- src/js/embed.js
+++ src/js/embed.js
@@ -21,7 +21,7 @@ window.init = function init(el, config) {
 
 function buildApp(resp) {
 
-    mode= getParameterByName("mode");
+    mode = getParameterByName("mode");
     data = resp.sheets.Sheet1;
 
     if (mode === null) {
@@ -32,15 +32,15 @@ function buildApp(resp) {
     //console.log(data);
     //console.log(_);
 
-    var i, html="", leaveHtml = "", personHtml, personTemplate = _.template(personHTML), personGroup = document.getElementById("person-group"), leaveGroup = document.getElementById("leaving-group"), status, title, previousTitle;
+    var i, html = "", leaveHtml = "", personHtml, personTemplate = _.template(personHTML), personGroup = document.getElementById("person-group"), leaveGroup = document.getElementById("leaving-group"), status, title, previousTitle;
 
-    for (i = 0; i<data.length; i++) {
+    for (i = 0; i < data.length; i++) {
 
         status = getStatusClass(data[i]["Status"]);
         previousTitle = "";
         title = data[i]["Title"];
 
-        if (status == "change" ) {
+        if (status == "change") {
             previousTitle = " was " + data[i]["Previous title"];
         }
         if (status == "leaving") {
@@ -49,54 +49,55 @@ function buildApp(resp) {
             title = "";
         }
 
-        personHtml = personTemplate({ photoSrc: data[i]["Photo"],
-								personName: data[i]["Name"],
-								personTitle: title,
-                                personPreviousTitle: previousTitle,
-                                status: status
-							});
-                            
+        personHtml = personTemplate({
+            photoSrc: data[i]["Photo"],
+            personName: data[i]["Name"],
+            personTitle: title,
+            personPreviousTitle: previousTitle,
+            status: status
+        });
+
         if (status == "leaving") {
             leaveHtml += personHtml;
         } else {
             html += personHtml;
         }
-        
+
     }
 
     personGroup.innerHTML = html;
     leaveGroup.innerHTML = leaveHtml;
-    
+
 }
 
-function getStatusClass( status ) {
+function getStatusClass(status) {
 
-    status=status.toLowerCase().trim();
+    status = status.toLowerCase().trim();
 
     switch (status) {
 
         case "change":
-        return "change";
-        break;
+            return "change";
+            break;
 
         case "new":
-        return "new";
-        break;
-       
+            return "new";
+            break;
+
         case "leaving":
-        return "leaving";
-        break;
-       
+            return "leaving";
+            break;
+
         default:
-        return "no-change";
-        break;
+            return "no-change";
+            break;
 
     }
 }
 
- function getParameterByName(name, url) {
+function getParameterByName(name, url) {
     if (!url) {
-      url = window.location.href;
+        url = window.location.href;
     }
     name = name.replace(/[\[\]]/g, "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
