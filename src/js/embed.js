b2e3968656c3c1afa42e0473c9fbcee419cdd6a4
Garry Blight <garry.blight@guardian.co.uk>
2017-06-06 14:30:31 +0100

    New colours, show more buttons, resizer

--- src/js/embed.js
+++ src/js/embed.js
@@ -5,11 +5,63 @@ import personHTML from './text/person.html!text'
 import _ from 'underscore'
 
 var mode = null, data = null;
+var eventTimeout, totalOut = 0, outButton, element, outClicked = false;
+
+/**
+ * The function that runs the event actions
+ */
+var checkOutButton = function () {
+
+    var numShowing = null;
+
+    var pageWidth = element.offsetWidth;
+	// handle the event...
+
+    // get width of page;
+
+    // get total of outs
+
+  
+
+    if (pageWidth < 400 ) {
+        numShowing = 9;
+    } else if (pageWidth >= 400 && pageWidth < 520) {
+        numShowing = 12;
+    } else if (pageWidth >= 520 && pageWidth < 860) {
+         numShowing = 18; // 18 - 5;
+         
+    } else {
+        numShowing = 9999999;
+    }
+
+    if (totalOut > numShowing && !outClicked) {
+        outButton.style.display = 'inline-block';
+    } else {
+        outButton.style.display = 'none';
+    }
+
+    console.log(pageWidth + "    "  + numShowing + "   " + totalOut);
+
+};
+
+/**
+ * Throttle events to only run at 15fps
+ */
+var eventThrottler = function () {
+	// ignore resize events as long as an actualResizeHandler execution is in the queue
+	if ( !eventTimeout ) {
+		eventTimeout = setTimeout(function() {
+			eventTimeout = null;
+			checkOutButton();
+		 }, 66);
+	}
+}; 
 
 window.init = function init(el, config) {
     iframeMessenger.enableAutoResize();
 
     el.innerHTML = embedHTML;
+    element = el;
 
     reqwest({
         url: 'https://interactive.guim.co.uk/docsdata/1lrMs23YPKWxRStLVLiVj407bvIJvlG_WZxBMkVYui6I.json',
@@ -47,6 +99,7 @@ function buildApp(resp) {
             //previousTitle = "Was " + data[i]["Previous title"];
             previousTitle = "was " + data[i]["Previous title"] + "";
             title = "";
+            totalOut ++;
         }
 
         personHtml = personTemplate({
@@ -67,6 +120,22 @@ function buildApp(resp) {
 
     personGroup.innerHTML = html;
     leaveGroup.innerHTML = leaveHtml;
+
+    document.getElementById("show-more-person-button").addEventListener("click", function(){
+    this.style.display = 'none';
+    document.getElementById("person-group").className = "person-group expanded";
+});
+
+ document.getElementById("show-more-leaving-button").addEventListener("click", function(){
+     this.style.display = 'none';
+    document.getElementById("leaving-group").className = "person-group expanded";
+    outClicked = true;
+});
+
+outButton = document.getElementById("show-more-leaving-button");
+checkOutButton();
+// Run the event listener
+window.addEventListener( 'resize', eventThrottler, false );
 
 }
 
@@ -106,3 +175,7 @@ function getParameterByName(name, url) {
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+
+
+
+
